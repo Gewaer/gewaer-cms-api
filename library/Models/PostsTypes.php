5ee2c163321e30d6867843e88499ef418568319e
Rafael White <rafaelwhite27@gmail.com>
2019-07-13 11:46:59 -0400

    Add Posts related models

---
+++ library/Models/PostsTypes.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Gewaer\Models;
+
+class PostsTypes extends BaseModel
+{
+    /**
+     * @var integer
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @var datetime
+     */
+    public $created_at;
+
+    /**
+     * @var datetime
+     */
+    public $updated_at;
+
+    /**
+     * @var integer
+     */
+    public $is_deleted;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSource('posts_types');
+    }
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource(): string
+    {
+        return 'posts_types';
+    }
+
+}
---
+++ library/Models/Tags.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace Gewaer\Models;
+
+class Tags extends BaseModel
+{
+    /**
+     * @var integer
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @var string
+     */
+    public $slug;
+
+    /**
+     * @var datetime
+     */
+    public $created_at;
+
+    /**
+     * @var datetime
+     */
+    public $updated_at;
+
+    /**
+     * @var integer
+     */
+    public $is_deleted;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSource('tags');
+    }
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource(): string
+    {
+        return 'tags';
+    }
+
+}
