5face46f57fc5a0816037ad8409ad87ca4680d72
Rafael White <rwhite@mctekk.com>
2019-07-29 11:38:22 -0400

    Add user auth if token is sent

--- library/Middleware/SiteMiddleware.php
+++ library/Middleware/SiteMiddleware.php
@@ -8,6 +8,9 @@ use Phalcon\Mvc\Micro\MiddlewareInterface;
 use Phalcon\Mvc\Micro;
 use Canvas\Exception\PermissionException;
 use Gewaer\Models\Sites;
+use Canvas\Traits\TokenTrait;
+use Baka\Auth\Models\Sessions;
+use Canvas\Models\Users;
 
 /**
  * Class AclMiddleware.
@@ -16,6 +19,8 @@ use Gewaer\Models\Sites;
  */
 class SiteMiddleware implements MiddlewareInterface
 {
+    use TokenTrait;
+
     /**
      * Validate if we are accessing a correct site content.
      *
@@ -26,6 +31,7 @@ class SiteMiddleware implements MiddlewareInterface
     public function call(Micro $api)
     {
         $request = $api->getService('request');
+        $config = $api->getService('config');
 
         if (!$request->hasHeader('SITE-KEY')) {
             throw new PermissionException('No Site Found');
@@ -53,6 +59,36 @@ class SiteMiddleware implements MiddlewareInterface
                 return $site->user;
             }
         );
+
+        if ($request->getBearerTokenFromHeader()) {
+
+            /**
+            * This is where we will find if the user exists based on
+            * the token passed using Bearer Authentication.
+            */
+            $token = $this->getToken($request->getBearerTokenFromHeader());
+
+            $api->getDI()->setShared(
+                'userData',
+                function () use ($config, $token, $request) {
+                    $session = new Sessions();
+                    //all is empty and is dev, ok take use the first user
+                    if (empty($token->getClaim('sessionId')) && strtolower($config->app->env) == Flags::DEVELOPMENT) {
+                        return Users::findFirst(1);
+                    }
+                    if (!empty($token->getClaim('sessionId'))) {
+                        //user
+                        if (!$user = Users::getByEmail($token->getClaim('email'))) {
+                            throw new UnauthorizedHttpException('User not found');
+                        }
+                        $ip = !defined('API_TESTS') ? $request->getClientAddress() : '127.0.0.1';
+                        return $session->check($user, $token->getClaim('sessionId'), (string) $ip, 1);
+                    } else {
+                        throw new UnauthorizedHttpException('User not found');
+                    }
+                }
+            );
+        }
 
         return true;
     }
