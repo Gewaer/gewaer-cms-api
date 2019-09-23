069118be2c91f2f0d144fd353a9f89634059d1c5
Rafael White <rwhite@mctekk.com>
2019-09-23 13:51:29 -0400

    Add onconstruct function and crud for rss

--- api/controllers/RssController.php
+++ api/controllers/RssController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Gewaer\Api\Controllers;
 
+use Gewaer\Models\Rss;
+
 /**
  * Class BaseController.
  *
@@ -12,13 +14,34 @@ namespace Gewaer\Api\Controllers;
  */
 class RssController extends BaseController
 {
+    /*
+       * fields we accept to create
+       *
+       * @var array
+       */
+    protected $createFields = ['site', 'url', 'rss_url', 'format'];
+
+    /*
+     * fields we accept to create
+     *
+     * @var array
+     */
+    protected $updateFields = ['site', 'url', 'rss_url', 'format'];
+
     /**
      * set objects.
      *
      * @return void
      */
-    public function getRss(): void
+    public function onConstruct()
     {
-        require '../../rss.php';
+        $this->model = new Rss();
+        $this->model->users_id = $this->userData->getId();
+        $this->model->companies_id = $this->userData->currentCompanyId();
+        $this->model->sites_id = $this->site->getId();
+
+        $this->additionalSearchFields = [
+            ['is_deleted', ':', '0']
+        ];
     }
 }
